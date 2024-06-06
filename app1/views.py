e81a5a9d06037658a784ef5c7af4a81d74516ca9
BUAA-makima <1242812772@qq.com>
2024-06-06 17:18:30 +0800

    查询公告时返回完整内容

--- app1/views.py
+++ app1/views.py
@@ -734,7 +734,8 @@ def notice_digest(request):
                 "title": n.title,
                 "tag": n.tag,
                 "state": n.read,
-                "id": str(n.id)
+                "id": str(n.id),
+                "content": n.content
             } for n in notices]
         })
 
